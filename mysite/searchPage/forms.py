b19645db9a3cc192f0ee5cf8702237bb99287175
Kushal Kumarasinghe <kushal@Kushals-MacBook-Air.local>
2018-05-18 23:32:09 -0400

    added time storage logic

--- mysite/searchPage/forms.py
+++ mysite/searchPage/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class ActivityForm(forms.Form):
     activity_name = forms.CharField(label='activity', max_length=500)
-    activity_duration = forms.IntegerField(label='duration')
+    activity_hours = forms.IntegerField(label='hours')
+    activity_minutes = forms.IntegerField(label='minutes')
--- mysite/searchPage/views.py
+++ mysite/searchPage/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Activity
 from .forms import ActivityForm
+import math
 
 def index(request):
-    form = ActivityForm()
+    form = ActivityForm(initial={
+        'activity_hours': 0,
+        'activity_minutes': 0,
+    })
     return render(request, "searchPage/home.html", {'form': form})
 
 
@@ -14,11 +18,40 @@ def results(request):
         if form.is_valid():
             new_activity = Activity(
                 name=form.cleaned_data['activity_name'],
-                duration=form.cleaned_data['activity_duration'],
+                duration=timeInSeconds(
+                    form.cleaned_data['activity_hours'],
+                    form.cleaned_data['activity_minutes'],
+                ),
             )
             new_activity.save()
         else:
             # show errors
+            pass
 
     activities = Activity.objects.all()
-    return render(request, "searchPage/results.html", {'activities': activities})
+    processed = []
+
+    for activity in activities:
+        hours, mins = formattedTime(activity.duration)
+        processed.append({
+            'name': activity.name,
+            'hours': hours,
+            'minutes': mins,
+        })
+
+    return render(request, "searchPage/results.html", {'activities': processed})
+
+
+def formattedTime(seconds):
+    # remove seconds
+    seconds -= seconds % 60
+
+    # calc mins and hours
+    mins = seconds / 60
+    hours = mins / 60
+
+    return math.floor(hours), math.floor(mins % 60)
+
+
+def timeInSeconds(hours, minutes):
+    return hours*3600 + minutes * 60
