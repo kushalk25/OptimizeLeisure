db5ad7102918062952be3daf2323075e18e734e0
Kushal Kumarasinghe <kushal@Kushals-MacBook-Air.local>
2018-05-19 01:05:32 -0400

    added redirect buttons and logic

--- mysite/searchPage/static/js/redirect.js
+++ mysite/searchPage/static/js/redirect.js
@@ -1,5 +1,25 @@
 
 $( document ).ready(function() {
-    // Javascript setup example for future use
-    console.log("imported some Javascript")
+  var resultsBtn = document.getElementById("resultsBtn");
+  if (resultsBtn) {
+    console.log("results button found")
+    resultsBtn.onclick = function clicked(){
+      console.log ("results button clicked")
+      window.location.replace("results");
+    }
+  } else {
+    console.log("no results button")
+  }
+
+  var homeBtn = document.getElementById("homeBtn");
+  if (homeBtn) {
+    console.log("home button found")
+    homeBtn.onclick = function clicked(){
+      console.log("home button clicked")
+      window.location.replace("/searchPage");
+    }
+  } else {
+    console.log("no home button")
+  }
+
 });
